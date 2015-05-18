b10008a60d65884a055f2c8d7caa2b7df367dbd7
Sven Michael Klose <pixel@copei.de>
2015-05-18 03:18:38 +0200

    UNCLIP has a variable range now.

--- src/wav2pwm.lisp
+++ src/wav2pwm.lisp
@@ -15,14 +15,14 @@
 (defun unsigned (x)
   (+ x (? (< 127 x) -128 128)))
 
-(defun unclip (x)
-  (+ 1 (integer (* (/ x 16) 14))))
+(defun unclip (x range)
+  (integer (+ (half (- 16 range)) (* (/ x 16) range))))
 
 (defun wav2pwm (out in-file &optional video)
   (alet (fetch-file in-file)
     (dotimes (i (length !))
       (unless (zero? (mod i 2))
-        (let sample (unclip (/ (unsigned (elt ! i)) 16))
+        (let sample (unclip (/ (unsigned (elt ! i)) 16) 14)
           (& (| (< sample 0)
                 (< 15 sample))
              (error "Sample ~A." sample))
