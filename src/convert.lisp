1c953ff06ac8cc1678cfe84701a5bb2d72ddf0ea
Sven Michael Klose <pixel@copei.de>
2015-12-19 14:46:40 +0100

    Utility functions to convert audio.

---
+++ src/convert.lisp
@@ -0,0 +1,25 @@
+(defun make-wav (name file)
+  (sb-ext:run-program "/usr/bin/mplayer"
+    (list "-vo" "null" "-vc" "null" "-ao" (+ "pcm:fast:file=obj/" name ".wav") file)
+    :pty cl:*standard-output*))
+
+(defun make-filtered-wav (name gain bass tv rate)
+  (sb-ext:run-program "/usr/bin/sox"
+    `(,(+ "obj/" name ".wav")
+      ,(+ "obj/" name "." (downcase (symbol-name tv)) ".filtered.wav")
+      ,@(& bass `("bass" ,bass))
+      "lowpass" ,(princ (half rate) nil)
+      ,@(& gain `("compand" "0.3,1" "6:-70,-60,-20" "-1" "-90" "0.2" "gain" ,gain)))
+    :pty cl:*standard-output*))
+
+(defun downsampled-audio-name (name tv)
+  (+ "obj/" name ".downsampled." (downcase (symbol-name tv)) ".wav"))
+
+(defun make-conversion (name tv rate)
+  (sb-ext:run-program "/usr/bin/sox"
+    (list (+ "obj/" name "." (downcase (symbol-name tv)) ".filtered.wav")
+          "-c" "1"
+          "-b" "16"
+          "-r" (princ rate nil)
+          (downsampled-audio-name name tv))
+    :pty cl:*standard-output*))
